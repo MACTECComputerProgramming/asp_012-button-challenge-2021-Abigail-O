31797ac834480ff43c9727e3665ad265ebc2b914
Abigail-O <oabi2305@gmail.com>
2021-09-23 13:06:34 -0500

    last push

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,18 +30,50 @@ namespace radio_button_challenge_2
             if (button1.IsChecked == true)
             {
                 answerLabel.Content = " Pencil, very reliable!";
+
+                pencil.Opacity = 100;
+
+                pen.Opacity = 0;
+
+                phone.Opacity = 0;
+
+                pad.Opacity = 0;
             }
             else if (button2.IsChecked == true)
             {
                 answerLabel.Content = " Pen, nice choice!";
+
+                pen.Opacity = 100;
+
+                pencil.Opacity = 0;
+
+                phone.Opacity = 0;
+
+                pad.Opacity = 0;
             }
             else if (button3.IsChecked == true)
             {
                 answerLabel.Content = " Phone, must be from the future!";
+
+                phone.Opacity = 100;
+
+                pencil.Opacity = 0;
+
+                pen.Opacity = 0;
+
+                pad.Opacity = 0;
             }
             else if (button4.IsChecked == true)
             {
                 answerLabel.Content = " Pad, welcome to 2021!";
+
+                pad.Opacity = 100;
+
+                pencil.Opacity = 0;
+
+                pen.Opacity = 0;
+
+                phone.Opacity = 0;
             }
 
 
